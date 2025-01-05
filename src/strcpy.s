7f271c0d817ee40e33aa60c881bfbe06df1811cd
Lok Eu Lee <lokeulee@gmail.com>
2025-01-05 19:14:26 +0800

    fix: strcpy not null terminating properly

--- src/strcpy.s
+++ src/strcpy.s
@@ -4,15 +4,16 @@ ft_strcpy:
     mov     rcx, rdi                  ; Save the original destination pointer
 
 strcpy_next:
-    cmp     byte [rdi], 0             ; Check if the current byte is null (end of string)
-    jz      strcpy_done               ; jump if equals to 0
-
     mov     al, byte [rsi]            ; Load the byte from the source string
     mov     byte [rdi], al            ; Store the byte in the destination string
+
+	cmp     byte [rdi], 0             ; Check if the current byte is null (end of string)
+    jz      strcpy_done               ; jump if equals to 0
+
     inc     rsi                       ; Move to the next byte in the source
     inc     rdi                       ; Move to the next byte in the destination
     jmp     strcpy_next               ; Continue looping
 strcpy_done
-
+ 	mov     byte [rdi], 0             ; Null-terminate the destination string
     mov     rax, rcx                  ; Return the original destination pointer
     ret								  ; Return from the function
